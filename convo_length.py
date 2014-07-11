563d57ff41e26a933c99234d19076e85df7bb8a3
cat <sbchou@gmail.com>
2014-07-11 16:58:53 -0400

    add prelim haxing on convo length and results. Note need to fetch all convos still (only gets first page on org) and also remove weird line break

---
+++ convo_length.py
@@ -0,0 +1,48 @@
+"""
+For an Organization, get info about discussion lengths
+@author soph
+"""
+
+from bs4 import BeautifulSoup
+import requests # The urllib2 module is bad, this is better.
+
+
+# First get all discussions
+base_url = "https://internet-party.loomio.org"
+r = requests.get(base_url)
+page = r.text
+soup = BeautifulSoup(page)
+
+titles = [t.text for t in soup.find_all('div', {'class':'discussion-title'})]
+links = soup.find_all('a', {'class': 'selector-discussion-link'})
+
+print "DISCUSSION_TITLE\tDISCUSSION_LINK\tNUM_TEXTS\tWORD_COUNT"
+    
+for title, link in zip(titles, links):
+    discussion_url = link.get('href')
+    r = requests.get(base_url + discussion_url)
+    page = r.text
+    soup = BeautifulSoup(page)
+
+    # Count the number of activities on this topic
+    responses = soup.find_all('li', {'class': 'activity-tem-container'})
+    num_responses = len(responses) 
+
+    # Count the number of text replies to this topic
+    text_responses = soup.find_all('span', {'class': 'activity-item-content word-break'})
+    texts = [t.text for t in text_responses if t.text]
+    num_texts = len(texts)
+
+    # Count the total length of text replies
+    word_count = sum([len(t.split()) for t in texts])
+
+    # ORG DISCUSSION_TITLE DISCUSSION_LINK NUM_TEXTS WORD_COUNT
+    print str(base_url) + "\t" + str(title) + "\t" + \
+        str(discussion_url) + "\t" + str(num_texts) + \
+        "\t" + str(word_count)
+
+# ISSUE: WE ONLY GET THE FIRST PAGE OF CONVERSATIONS ON THE 
+# ORG HOMEPAGE
+
+
+
